8b179e9bfa12c87c7aa5edbe68568f8fe46fac6b
Manju-sai <istatimanjusai@gmail.com>
2025-01-09 15:13:17 +0530

    Add files via upload
    added sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO movie_details (movie_title, movie_language, movie_release_date, movie_image_url) 
+VALUES 
+('The Shawshank Redemption', 'English', '1994-09-22', 'https://example.com/images/shawshank.jpg'),
+('The Godfather', 'English', '1972-03-24', 'https://example.com/images/godfather.jpg'),
+('The Dark Knight', 'English', '2008-07-18', 'https://example.com/images/dark_knight.jpg'),
+('Pulp Fiction', 'English', '1994-10-14', 'https://example.com/images/pulp_fiction.jpg'),
+('Forrest Gump', 'English', '1994-07-06', 'https://example.com/images/forrest_gump.jpg'),
+('Inception', 'English', '2010-07-16', 'https://example.com/images/inception.jpg');
